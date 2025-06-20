5e01c480249d60128f740f3b3fe1f0a595e31eb9
Anand Kishore <anandpratyush272@gmail.com>
2025-06-20 13:09:27 +0530

    fixed token issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = async (userId, res) => {
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "None",
         secure: process.env.NODE_ENV !== "development"
     });
 
