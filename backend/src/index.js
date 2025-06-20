01b12829c3618be512f27641e1bb010cdacb9043
Anand Kishore <anandpratyush272@gmail.com>
2025-06-20 12:23:18 +0530

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,10 +17,11 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.NODE_ENV === "production"
-        ? "https://vchatappl.onrender.com"
+        ? ["https://vchat-app-6.onrender.com", "https://vchatappl.onrender.com"]
         : "http://localhost:5173",
     credentials: true
-}))
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
